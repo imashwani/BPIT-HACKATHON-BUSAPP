00ce1f938080d6711748c33763ddfea9d6814812
imashwani <ashwani.p30@gmail.com>
2018-04-05 01:52:18 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ashwani/buswaliapp/GameActivity.java
@@ -0,0 +1,17 @@
+package com.example.ashwani.buswaliapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+
+
+
+    }
+}
